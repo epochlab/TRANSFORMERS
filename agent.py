d951d9e0e0d2cdfdf0ce399d0c08db45058a9121
James Roberts <JRoberts86@gmail.com>
2023-12-24 22:39:43 +0000

    Update test agent vision

--- agent.py
+++ agent.py
@@ -1,44 +1,49 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
+from sentencepiece import SentencePieceProcessor
+
 from arch.llm import Llama
 from arch.vision import Vision
 from arch.vectordb import VectorDB
-from arch.utils import device_mapper, load_config, albedo, url2image
+from arch.utils import device_mapper, albedo, url2image
 
 DEVICE = device_mapper()
 print(f"Device: {str(DEVICE).upper()}")
 
-BLIP = "/Users/James/Documents/blip-image-captioning-large/"
-MISTRAL = "/mnt/artemis/library/weights/mistral/OpenHermes-2-Mistral-7B"
+BLIP_PATH = "/Users/James/Documents/blip-image-captioning-large"
+MODEL_DIR = "/Users/James/Documents/OpenHermes-2-Mistral-7B"
+TOKENIZER_PATH = (MODEL_DIR if Path(MODEL_DIR).is_dir() else Path(MODEL_DIR).parent) + "/tokenizer.model"
 
 def main():
-    db = vectorDB(DEVICE)
-    LLM = Llama(MISTRAL, DEVICE)
-    v1 = Vision(BLIP, DEVICE)
+    db = VectorDB(DEVICE)
+    tokenizer = SentencePieceProcessor(model_file=str(TOKENIZER_PATH))
+    print(f"Vocab Size: {tokenizer.vocab_size()}")
 
-    system = load_config('profiles.yml')['creation']
-    print(albedo(system['call'], 'green'))
+    # LLM = Llama(MODEL_DIR, tokenizer, DEVICE)
+    v1 = Vision(BLIP_PATH, DEVICE)
 
-    # Replace with token_wrapper
-    IM_START = LLM.tokenizer.bos_token
-    IM_END = LLM.tokenizer.eos_token
-
-    en = "How to bake a loaf of bread?"
+    en = "Prometheus stole fire from the gods and gave it to man."
     db.push(en)
 
-    log = [IM_START] + [system['call']] + [IM_END] + [IM_START] + [en] + [IM_END] # <- Script???
-    print(albedo(log, 'red')) # Print chat history
+    # Replace with token_wrapper
+    IM_START = tokenizer.bos_id()
+    IM_END = tokenizer.eos_id()
 
-    toks = LLM.encode(log)
-    new_tok = LLM.generate(toks, max_length=1024, temp=0.7)
-    response = LLM.decode(new_tok, skip_special=True)
+    log = [IM_START] + tokenizer.encode(en) + [IM_END]
+    print(albedo(log, 'red')) # Print chat history
+    print(albedo(tokenizer.decode(log), "green"))
+
+    # toks = LLM.encode(log)
+    # new_tok = LLM.generate(toks, max_length=1024, temp=0.7)
+    # response = LLM.decode(new_tok, skip_special=True)
+
+    url = 'https://images.newscientist.com/wp-content/uploads/2017/02/08190042/gettyimages-615305114.jpg'
+    raw_image = url2image(url)
+    response = v1.witness(raw_image)
 
     db.push(response)
     print(db.pull(['documents']))
-
-    url = 'https://hips.hearstapps.com/hmg-prod/images/is-santa-claus-real-1632187377.jpg?'
-    raw_image = url2image(url)
-    print(v1.witness(raw_image))
 
     # index = faiss.IndexFlatL2(embedding.shape[1])
     # index.add(embedding.numpy())
