728f87cb0af646c48e20eba51b1df22171d06a23
James Roberts <JRoberts86@gmail.com>
2023-12-24 21:12:30 +0000

    Clean up, push models to agent-level

--- arch/vision.py
+++ arch/vision.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-import torch
 from transformers import BlipProcessor, BlipForConditionalGeneration
-from utils import url2image, albedo
-
-DEVICE = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-print(f"Device: {str(DEVICE).upper()}")
-
-MODEL_DIR = "/Users/James/Documents/blip-image-captioning-large/"
 
 class Vision():
     def __init__(self, model_dir, device):
@@ -16,6 +9,6 @@ class Vision():
         self.device = device
 
     def witness(self, img, max_length=128):
-        inputs = self.processor(img, return_tensors="pt").to(DEVICE)
+        inputs = self.processor(img, return_tensors="pt").to(self.device)
         out = self.model.generate(**inputs, max_new_tokens=max_length)
         return self.processor.decode(out[0], skip_special_tokens=True)
