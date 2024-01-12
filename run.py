5ee0110093035ece9b98126bb32687c8540d7c33
epochlab <jroberts86@gmail.com>
2024-01-12 22:05:53 +0000

    Resolve chat completion

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from arch.llama.llm import Llama
-from arch.utils import device_mapper, load_config
+from arch.utils import device_mapper, load_config, chat_playback, albedo
 
 from typing import List, Literal, TypedDict
 
@@ -22,30 +22,37 @@ B_SYS, E_SYS = "<<SYS>>", "<</SYS>>"
 def main():
     LLM = Llama.build(ckpt_dir=MODEL_PATH, tokenizer_path=TOKENIZER_PATH, max_seq_len=512, max_batch_size=8, device=DEVICE)
 
-    dialogs: List[Dialog] = [[{"role": "system", "content": load_config('profiles.yml')['jasmine']}, 
-                              {"role": "user", "content": "How fast is an F16?"}]]
+    system_call = load_config('profiles.yml')['jasmine']
+    dialogs: List[Dialog] = [[{"role": "system", "content": system_call}]]
 
-    toks = []
-    for dialog in dialogs:
+    print("Type 'exit()' to cancel.")
+    print(f"{albedo(system_call, 'green')}")
+    print("\n==================================\n") 
+
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() == "exit()": break
+        dialogs[-1].append({"role": "user", "content": user_input})
+
+        dialog = dialogs[-1]
+
         if dialog[0]["role"] == "system":
             dialog = [{"role": dialog[1]["role"], "content": B_SYS + dialog[0]["content"] + E_SYS + dialog[1]["content"]}] + dialog[2:]
 
         dialog_toks: List[int] = sum([LLM.tokenizer.encode(f"{B_INST} {(prompt['content']).strip()} {E_INST} {(answer['content']).strip()}", bos=True, eos=True,) for prompt, answer in zip(dialog[::2], dialog[1::2],)],[],)
         dialog_toks += LLM.tokenizer.encode(f"{B_INST} {(dialog[-1]['content']).strip()} {E_INST}", bos=True, eos=False)
-        toks.append(dialog_toks)
+        toks = [dialog_toks]
 
         logprobs = False
-        new_toks, logprobs = LLM.generate(prompt_tokens=toks, max_gen_len=None, temperature=0.6, top_p=0.9, logprobs=logprobs)
+        new_toks, _ = LLM.generate(prompt_tokens=toks, max_gen_len=None, temperature=0.6, top_p=0.9, logprobs=logprobs)
 
         if logprobs:
-            results = [{"generation": {"role": "assistant", "content": LLM.tokenizer.decode(t)}, "tokens": [LLM.tokenizer.decode(x) for x in t], "logprobs": logprobs_i,} for t, logprobs_i in zip(new_toks, logprobs)]
+            result = [{"generation": {"role": "assistant", "content": LLM.tokenizer.decode(t)}, "tokens": [LLM.tokenizer.decode(x) for x in t], "logprobs": logprobs_i,} for t, logprobs_i in zip(new_toks, logprobs)]
         else:
-            results = [{"generation": {"role": "assistant", "content": LLM.tokenizer.decode(t)}} for t in new_toks]
+            result = {"generation": {"role": "assistant", "content": LLM.tokenizer.decode(new_toks[0])}}
 
-    for dialog, result in zip(dialogs, results):
-        for msg in dialog:
-            print(f"{msg['role'].capitalize()}: {msg['content']}")
-        print(f">{result['generation']['content']}")
+        chat_playback(f"> {result['generation']['content']}")
+        dialogs[-1].append(result['generation'])
 
 if __name__ == "__main__":
     main()
