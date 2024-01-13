cdd7b912733acb52cb3ca6f103467d3ac0c85dac
epochlab <jroberts86@gmail.com>
2024-01-13 00:11:16 +0000

    Publish Llama

--- arch/utils.py
+++ arch/utils.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import yaml, time, requests, random, string
-import torch
-import numpy as np
+import yaml, time, requests
 from io import StringIO
 from contextlib import redirect_stdout
+
+import torch
+import numpy as np
 from PIL import Image
 
 def device_mapper():
@@ -12,25 +13,21 @@ def device_mapper():
     elif torch.backends.mps.is_available(): return torch.device("mps")
     else: return torch.device("cpu")
 
-def load_config(file):
+def load_config(file: str):
     with open(file) as f:
         return yaml.full_load(f)
 
-def url2image(url):
+def url2image(url: str):
     response = requests.get(url, stream=True)
     if response.status_code == 200:
         return Image.open(response.raw).convert('RGB')
     else:
         print(f"Error: URL returned status code {response.status_code}")
 
-def image2tensor(file):
+def image2tensor(file: str):
     sample = np.array(Image.open(file).convert('RGB'))
     # To Do: Normalise
     return torch.tensor(sample.transpose((2, 0, 1))).float()
-
-def alphanumeric(length):
-    chars = string.ascii_letters + string.digits
-    return ''.join((random.choice(chars) for i in range(length)))
 
 def albedo(text, clr='white'):
     color_index = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'].index(clr.lower())
@@ -39,7 +36,6 @@ def albedo(text, clr='white'):
     return f"{color_code}{text}\u001b[0m"
 
 def chat_playback(response):
-    # bucket = response.replace('\n', "")
     bucket = ' '.join(response.split())
     for word in bucket:
         time.sleep(0.01)
@@ -48,8 +44,8 @@ def chat_playback(response):
 
 def coder(response):
     # USER WARNING: Consider AI safety before execution.
-    if '```python' in response:
-        python_code = response.split('```python\n')[1].split("```")[0]
+    if '```' in response:
+        python_code = response.split('```\n')[1].split("```")[0]
         print(albedo(f"CODE DETECTED:\n\n{python_code}", 'red'))
 
         if input("Do you want to EXECUTE? (Y/n) ").lower() == 'y':
--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from arch.llama.llm import Llama
-from arch.utils import device_mapper, load_config, chat_playback, albedo
-
 from typing import List, Literal, TypedDict
+
+from arch.llama.llm import Llama
+from arch.utils import device_mapper, load_config, chat_playback, coder, albedo
 
 DEVICE = device_mapper()
 print(f"Device: {str(DEVICE).upper()}")
@@ -22,8 +22,8 @@ B_SYS, E_SYS = "<<SYS>>", "<</SYS>>"
 def main():
     LLM = Llama.build(ckpt_dir=MODEL_PATH, tokenizer_path=TOKENIZER_PATH, max_seq_len=512, max_batch_size=8, device=DEVICE)
 
-    system_call = load_config('profiles.yml')['jasmine']
-    dialogs: List[Dialog] = [[{"role": "system", "content": system_call}]]
+    system_call = load_config('profiles.yml')['quentin']
+    log: List[Dialog] = [[{"role": "system", "content": system_call}]]
 
     print("Type 'exit()' to cancel.")
     print(f"{albedo(system_call, 'green')}")
@@ -32,10 +32,9 @@ def main():
     while True:
         user_input = input("User: ")
         if user_input.lower() == "exit()": break
-        dialogs[-1].append({"role": "user", "content": user_input})
+        log[-1].append({"role": "user", "content": user_input})
 
-        dialog = dialogs[-1]
-
+        dialog = log[-1]
         if dialog[0]["role"] == "system":
             dialog = [{"role": dialog[1]["role"], "content": B_SYS + dialog[0]["content"] + E_SYS + dialog[1]["content"]}] + dialog[2:]
 
@@ -44,15 +43,18 @@ def main():
         toks = [dialog_toks]
 
         logprobs = False
-        new_toks, _ = LLM.generate(prompt_tokens=toks, max_gen_len=None, temperature=0.6, top_p=0.9, logprobs=logprobs)
+        new_toks, tok_logprobs = LLM.generate(prompt_tokens=toks, max_gen_len=None, temperature=0.6, top_p=0.9, logprobs=logprobs)
 
         if logprobs:
-            result = [{"generation": {"role": "assistant", "content": LLM.tokenizer.decode(t)}, "tokens": [LLM.tokenizer.decode(x) for x in t], "logprobs": logprobs_i,} for t, logprobs_i in zip(new_toks, logprobs)]
+            result = [{"generation": {"role": "assistant", "content": LLM.tokenizer.decode(t)}, "tokens": [LLM.tokenizer.decode(x) for x in t], "logprobs": logprobs_i,} for t, logprobs_i in zip(new_toks[0], tok_logprobs)]
         else:
             result = {"generation": {"role": "assistant", "content": LLM.tokenizer.decode(new_toks[0])}}
 
+        log[-1].append(result['generation'])
+        # print(albedo(dialog, "red")) # Print chat history
+
         chat_playback(f"> {result['generation']['content']}")
-        dialogs[-1].append(result['generation'])
+        coder(result['generation']['content'])
 
 if __name__ == "__main__":
     main()
